d18767128dd4515478d858052b46095609b44b4d
Ryan Corbett <ryan@rydestudio.com>
2018-11-07 09:09:02 -0800

    added favicon

--- index.php
+++ index.php
@@ -2,8 +2,17 @@
 <html ng-app="mainApp">
     <head>
         <title>{{ngMeta.title}}</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="shortcut icon" href="favicon.ico" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+
         <meta name="keywords" content="{{ngMeta.keywords}}" />
 
         <meta property="og:title" content="{{ngMeta.title}}" />
